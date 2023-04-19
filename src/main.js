05b6cdc710cf5cd1dd1940468a5d44950f8d842b
Sandy García <sandy.garcia9@misena.edu.co>
2023-04-19 08:25:26 -0500

    New configuration

--- src/main.js
+++ src/main.js
@@ -1,5 +1,47 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+import '@quasar/extras/material-icons-round/material-icons-round.css'
+import '@quasar/extras/material-icons-sharp/material-icons-sharp.css'
+import '@quasar/extras/material-symbols-outlined/material-symbols-outlined.css'
+import '@quasar/extras/material-symbols-rounded/material-symbols-rounded.css'
+import '@quasar/extras/material-symbols-sharp/material-symbols-sharp.css'
+import '@quasar/extras/mdi-v7/mdi-v7.css'
+import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
+import '@quasar/extras/fontawesome-v6/fontawesome-v6.css'
+import '@quasar/extras/ionicons-v4/ionicons-v4.css'
+import '@quasar/extras/eva-icons/eva-icons.css'
+import '@quasar/extras/themify/themify.css'
+import '@quasar/extras/line-awesome/line-awesome.css'
+import '@quasar/extras/bootstrap-icons/bootstrap-icons.css'
+
+import 'quasar/src/css/index.sass'
+
+
+
+import App from './App.vue'
+import { createApp } from 'vue'
+import { Quasar } from 'quasar'
+import {createRouter, createWebHashHistory} from 'vue-router'
+import {routes} from "../routes/routes.js"
+import { createPinia } from "pinia"
+const app = createApp(App)
+
+const pinia = createPinia()
+
+app.use(Quasar, {
+    plugins: {}, 
+})
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+})
+
+
+app.use(pinia)
+
+app.use(router)
+app.mount('#app')

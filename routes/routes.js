22b795efd3ad36b6f0d5cb75ca74c0cd7a77fe1f
Karen Cardenas <112130685+karenporrass@users.noreply.github.com>
2023-04-21 17:11:41 -0500

    rename

--- routes/routes.js
+++ routes/routes.js
@@ -2,15 +2,15 @@ import Login from "../src/components/Login.vue"
 // import Register from "../src/components/Register.vue"
 // import Card from "../src/components/Card.vue"
 import Home from "../src/components/Home.vue"
-import pedidos from "../src/components/orders/pedidos.vue"
-import pedidosDos from "../src/components/orders/pedidos2.vue"
+import orders from "../src/components/orders/Orders.vue"
+import tablesOrders from "../src/components/orders/TablesOrders.vue"
 import HomeCosts from "../src/components/Costs/HomeCosts.vue"
 
 
 export const routes=[
     {path:"/", component:Login},
     {path:"/home", component:Home},
-    {path:"/pedidos", component:pedidos},
-    {path:"/pedidosDos", component:pedidosDos},
+    {path:"/pedidos", component:orders},
+    {path:"/pedidosDos", component:tablesOrders},
     {path:"/HomeCostos", component: HomeCosts}
 ]

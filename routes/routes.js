047c617c58bd90542fd3bc50240476d0372a3d8b
Karen Cardenas <112130685+karenporrass@users.noreply.github.com>
2023-04-24 11:41:31 -0500

    create components maintenance

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,21 @@
-import Login from "../src/components/Login.vue"
-import Home from "../src/components/Home.vue"
-import orders from "../src/components/orders/Orders.vue"
-import tablesOrders from "../src/components/orders/TablesOrders.vue"
-import homeCosts from "../src/components/Costs/HomeCosts.vue"
-import pays from "../src/components/Costs/Pays.vue"
-import mounthlyExpenses from "../src/components/Costs/MonthlyExpenses.vue"
-import occasionalExpenses from "../src/components/Costs/OccasionalExpenses.vue"
+//INITIAL
+import Login from '../src/components/Login.vue'
+import Home from '../src/components/Home.vue'
+//ORDERS
+import orders from '../src/components/orders/Orders.vue'
+import tablesOrders from '../src/components/orders/TablesOrders.vue'
+//COSTS
+import homeCosts from '../src/components/Costs/HomeCosts.vue'
+import pays from '../src/components/Costs/Pays.vue'
+import mounthlyExpenses from '../src/components/Costs/MonthlyExpenses.vue'
+import occasionalExpenses from '../src/components/Costs/OccasionalExpenses.vue'
+//MAINTENANCE
+import people from '../src/components/Maintenance/People.vue'
+import farmRegistry from '../src/components/Maintenance/FarmRegistry.vue'
+import stages from '../src/components/Maintenance/Stages.vue'
+import support from '../src/components/Maintenance/Support.vue'
+import typePackaging from '../src/components/Maintenance/TypePackaging.vue'
+import lots from '../src/components/Maintenance/Lots.vue'
 
 export const routes=[
     {path:"/", component:Login},
@@ -15,7 +25,11 @@ export const routes=[
     {path:"/homeCostos", component: homeCosts},
     {path:"/pagos", component: pays},
     {path:"/gastosMensuales", component: mounthlyExpenses},
-    {path:"/gastosOcasionales", component: occasionalExpenses }
-    
-
+    {path:"/gastosOcasionales", component: occasionalExpenses},
+    {path:"/personas", component:people},
+    {path: "/registroFincas", component: farmRegistry},
+    {path: "/etapas", component: stages},
+    {path: "/soporte", component: support},
+    {path: "tipoEmpaques", component: typePackaging},
+    {path: "lotes", component:lots}
 ]

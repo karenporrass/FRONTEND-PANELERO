8522b763a2f3454dd5d9cca82736754129626714
AngelB10 <131167121+AngelB10@users.noreply.github.com>
2023-04-26 11:58:30 -0500

    home carrusel

--- src/components/Costs/HomeCosts.vue
+++ src/components/Costs/HomeCosts.vue
@@ -12,7 +12,7 @@
         <div class="row q-mt-xl">
             <div class="col-2"></div>
             <div class="col-2 q-mt-xl ">
-                <q-card class="my-card  shadow-8 " style="border: solid; border-color: darkgreen; border-radius: 15px;">
+                <q-card class="my-card  shadow-8 q-py-lg" style="border: solid; border-color: darkgreen; border-radius: 15px;">
                     <q-card-section>
                         <div style="margin: 0px auto; width: 20%; ">
                             <span class="material-symbols-outlined" style="font-size: 50px; color: black; font-weight:bold;">
@@ -20,7 +20,7 @@
                         </span></div>
                         
                     </q-card-section>
-                    <q-separator class="bg-green-10 " />
+                    <q-separator class="bg-green-10 " style="height: 2px;" />
 
     
 
@@ -32,11 +32,11 @@
             <div class="col-1"></div>
 
             <div class="col-2 text-center  q-mt-xl ">
-                <q-card class="my-card  shadow-8" style="border: solid; border-color: darkgreen; border-radius: 15px;">
+                <q-card class="my-card  shadow-8 q-py-lg" style="border: solid; border-color: darkgreen; border-radius: 15px;">
                     <q-card-section>
                         <div class="text-center"><i class="fa-solid fa-money-bill" style="font-size: 50px;"></i></div>
                     </q-card-section>
-                    <q-separator class="bg-green-10" />
+                    <q-separator class="bg-green-10" style="height: 2px;"/>
 
                  
 
@@ -49,11 +49,11 @@
             </div>
             <div class="col-1"></div>
             <div class="col-2 text-center  q-mt-xl ">
-                <q-card class="my-card  shadow-8 " style="border: solid; border-color: darkgreen; border-radius: 15px;">
+                <q-card class="my-card  shadow-8 q-py-lg" style="border: solid; border-color: darkgreen; border-radius: 15px;">
                     <q-card-section>
                         <div class="text-center"><i class="fa-solid fa-money-bill" style="font-size: 50px;"></i></div>
                     </q-card-section>
-                    <q-separator class="bg-green-10" />
+                    <q-separator class="bg-green-10" style="height: 2px;" />
 
                   
                     <router-link to="/gastosOcasionales" style="text-decoration: none;" class="text-dark" >
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,38 @@
 <template>
     <div>
         <div class="row">
-            <div class="col-4"></div>
-            <div class="col-4 text-h3 text-weight-bolder text-center">BIENVENIDO</div>
-            <div class="col-4"></div>
+            
+            <div class="col-12 text-h3 text-weight-bolder text-center"  style="padding: 0px; ">
+                <div style="padding: 0px; ">
+    <q-carousel
+      animated
+      v-model="slide"
+      arrows
+      navigation
+      infinite
+    >
+      <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg" />
+      <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg" />
+      <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg" />
+      <q-carousel-slide :name="4" img-src="https://cdn.quasar.dev/img/quasar.jpg" />
+    </q-carousel>
+  </div>
+            </div>
+           
         </div>
-    </div>
-</template>
 
-<style></style>
+      
+  
+</div>
+
+    
+</template>
+<script setup>
+import { ref } from 'vue'
+let slide = ref(1)
+
+</script>
+
+
+ 
+

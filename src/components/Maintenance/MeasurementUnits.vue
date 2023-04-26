c48eed0fcebd3ed0356c0094cbdf41cc6c76dd32
karenporrass <112130685+karenporrass@users.noreply.github.com>
2023-04-26 08:21:02 -0500

    conection

--- src/components/Maintenance/MeasurementUnits.vue
+++ src/components/Maintenance/MeasurementUnits.vue
@@ -1,12 +1,116 @@
 <template>
     <div>
-        <div class="row">
-            <div class="col-4"></div>
-            <div class="col-4 q-mt-lg q-mb-sm text-center text-weight-bolder text-h4">
-                UNIDADES DE MEDIDA
+        <div class="row q-mt-md">
+            <div class="col-1"></div>
+            <div class="col-10  text-center">
+                <div style="font-size:xx-large;" class="text-weight-bolder">TIPO DE EMPAQUES</div>
             </div>
-            <div class="col-4"></div>
+            <div class="col-1"></div>
         </div>
-        <hr class="bg-green-10 q-mb-xl" style="width: 70%; height: 2px" />
+        <q-separator class="q-my-md  bg-green-10" style="height: 2px; margin-left: 100px; margin-right: 100px;" />
+        <div class="row ">
+            <div class="col-1"></div>
+            <div class="col-10 ">
+                <q-btn class="bg-green-10 text-white" @click="prompt = true">Crear nuevo tipo de empaque</q-btn>
+            </div>
+            <div class="col-1"></div>
         </div>
+        <!-- TABLE INFO -->
+       <div class="row q-mt-md">
+            <div class="col-1"></div>
+            <div class="col-10 ">
+                <q-table style="height: 400px" flat bordered  :rows="rows" :columns="columns" row-key="index"
+                    virtual-scroll v-model:pagination = "pagination"  :rows-per-page-options="[0]" />
+            </div>
+            <div class="col-1"></div>
+        </div> 
+
+        <q-dialog v-model="prompt">
+            <q-card >
+              <q-card-section class="bg-green-10">
+                <h5 class="q-mt-sm q-mb-sm text-white text-center text-weight-bold">
+                  DILIGENCIA LA INFORMACIÓN
+                </h5>
+              </q-card-section>
+              <div class="q-pa-md " >
+                <div>
+                    <q-input  filled type="text" v-model="name" label="Digite el nombre del empaque"></q-input>
+                  <q-input filled type="number" v-model="maxWeight" label="Peso maximo"></q-input>
+                  <q-input  filled type="numeber" v-model="units" label="Digite las unidades por caja"></q-input>
+                  
+
+                  <div>
+                    <br />
+                    <q-btn  label="guardar" class="text-white bg-green-10"  />
+                    <q-btn class="q-ml-md" label="cerrar" v-close-popup />
+                  </div>
+                </div>
+              </div>
+            </q-card>
+          </q-dialog>
+    </div> 
 </template>
+  
+<script setup>
+import {ref} from "vue"
+let prompt = ref(false)
+let pagination = ref({
+        rowsPerPage: 0
+      })
+      let columns = ref([
+  {name: 'index',label: 'NOMBRE EMPAQUE',field: 'index',align: 'center'},
+  {name: 'name',required: true,label: 'PESO MAXIMO lb',align: 'center',field: row => row.name,format: val => `${val}`,sortable: true},
+  { name: 'calories', align: 'center', label: 'UNIDADES POR CAJA', field: 'calories',align: 'center', sortable: true },
+  
+])
+
+ 
+let rows= ref( [
+  {
+    name: 'Frozen Yogurt',
+    calories: 159,
+  },
+  {
+    name: 'Ice cream sandwich',
+    calories: 237,
+  },
+  {
+    name: 'Eclair',
+    calories: 262,
+  },
+  {
+    name: 'Cupcake',
+    calories: 305,
+  },
+  {
+    name: 'Gingerbread',
+    calories: 356,
+  },
+  {
+    name: 'Jelly bean',
+    calories: 375,
+  },
+  {
+    name: 'Lollipop',
+    calories: 392,
+  },
+  {
+    name: 'Honeycomb',
+    calories: 408,
+  },
+  {
+    name: 'Donut',
+    calories: 452,
+  },
+  {
+    name: 'KitKat',
+    calories: 518,
+  }
+])
+
+rows.value.forEach((row, index) => {
+  row.index = index
+})
+
+
+</script>

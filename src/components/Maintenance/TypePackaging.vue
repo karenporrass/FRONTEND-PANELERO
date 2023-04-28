bdcce716331f6da0699039b4e854de66160af88c
Sandy García <sandy.garcia9@misena.edu.co>
2023-04-28 08:30:50 -0500

    update table

--- src/components/Maintenance/TypePackaging.vue
+++ src/components/Maintenance/TypePackaging.vue
@@ -1,58 +1,74 @@
 <template>
-    <div>
-        <div class="row q-mt-md">
-            <div class="col-1"></div>
-            <div class="col-10  text-center">
-                <div style="font-size:xx-large;" class="text-weight-bolder">TIPO DE EMPAQUES</div>
-            </div>
-            <div class="col-1"></div>
-        </div>
-        <q-separator class="q-my-md  bg-green-10" style="height: 2px; margin-left: 100px; margin-right: 100px;" />
-        <div class="row ">
-            <div class="col-1"></div>
-            <div class="col-10 ">
-                <q-btn class="bg-green-10 text-white" @click="prompt = true, getTypePackaing()">Crear nuevo tipo de empaque</q-btn>
-            </div>
-            <div class="col-1"></div>
-        </div>
-        <!-- TABLE INFO -->
-       <div class="row q-mt-md">
-            <div class="col-1"></div>
-            <div class="col-10 ">
-                <q-table style="height: 400px" flat bordered  :rows="rows" :columns="columns" row-key="index"
-                    virtual-scroll v-model:pagination = "pagination"  :rows-per-page-options="[0]" />
-            </div>
-            <div class="col-1"></div>
-        </div> 
+  <div>
+    <div class="row q-mt-md">
+      <div class="col-1"></div>
+      <div class="col-10  text-center">
+        <div style="font-size:xx-large;" class="text-weight-bolder">TIPO DE EMPAQUES</div>
+      </div>
+      <div class="col-1"></div>
+    </div>
+    <q-separator class="q-my-md  bg-green-10" style="height: 2px; margin-left: 100px; margin-right: 100px;" />
+    <div class="row ">
+      <div class="col-1"></div>
+      <div class="col-10 ">
+        <q-btn class="bg-green-10 text-white" @click="prompt = true, getTypePackaing()">Crear nuevo tipo de
+          empaque</q-btn>
+      </div>
+      <div class="col-1"></div>
+    </div>
+    <!-- TABLE INFO -->
+    <div class="row q-mt-md">
+      <div class="col-1"></div>
+      <div class="col-10 ">
+        <q-table style="height: 400px" flat bordered :rows="rows" :columns="columns" row-key="index" virtual-scroll
+          v-model:pagination="pagination" :rows-per-page-options="[0]">
 
-        <q-dialog v-model="prompt">
-            <q-card >
-              <q-card-section class="bg-green-10">
-                <h5 class="q-mt-sm q-mb-sm text-white text-center text-weight-bold">
-                  DILIGENCIA LA INFORMACIÓN
-                </h5>
-              </q-card-section>
-              <div class="q-pa-md " >
-                <div>
-                    <q-input filled type="text" v-model="name" label="Digite el nombre del empaque"></q-input>
-                  <q-input filled type="number" v-model="maxWeight" label="Peso maximo"></q-input>
-                  <q-input filled type="number" v-model="units" label="Digite las unidades por caja"></q-input>
-                  
 
-                  <div>
-                    <br />
-                    <q-btn  label="guardar" class="text-white bg-green-10" @click="postTypePackaing()" />
-                    <q-btn class="q-ml-md" label="cerrar" v-close-popup />
-                  </div>
-                </div>
+          <template v-slot:body-cell-options="props" >
+            <q-td :props="props">
+              <div >
+                <q-btn round icon="edit" size="xs" color="primary" ></q-btn>
+                <q-btn round icon="edit" size="xs" color="primary"></q-btn>
               </div>
-            </q-card>
-          </q-dialog>
-    </div> 
+            </q-td>
+            
+          </template>
+
+
+
+        </q-table>
+      </div>
+      <div class="col-1"></div>
+    </div>
+
+    <q-dialog v-model="prompt">
+      <q-card>
+        <q-card-section class="bg-green-10">
+          <h5 class="q-mt-sm q-mb-sm text-white text-center text-weight-bold">
+            DILIGENCIA LA INFORMACIÓN
+          </h5>
+        </q-card-section>
+        <div class="q-pa-md ">
+          <div>
+            <q-input filled type="text" v-model="name" label="Digite el nombre del empaque"></q-input>
+            <q-input filled type="number" v-model="maxWeight" label="Peso maximo"></q-input>
+            <q-input filled type="number" v-model="units" label="Digite las unidades por caja"></q-input>
+
+
+            <div>
+              <br />
+              <q-btn label="guardar" class="text-white bg-green-10" @click="postTypePackaing()" />
+              <q-btn class="q-ml-md" label="cerrar" v-close-popup />
+            </div>
+          </div>
+        </div>
+      </q-card>
+    </q-dialog>
+  </div>
 </template>
   
 <script setup>
-import {ref, onMounted} from 'vue'
+import { ref, onMounted } from 'vue'
 import axios from 'axios';
 
 let prompt = ref(false)
@@ -60,23 +76,26 @@ let name = ref("")
 let maxWeight = ref()
 let units = ref()
 let pagination = ref({
-        rowsPerPage: 0
-      })
+  rowsPerPage: 0
+})
 let columns = ref([
-{ name: 'index', label: '#',field: 'index'},
-  {name: 'name',label: 'NOMBRE EMPAQUE',field: 'name',align: 'center'},
-  {name: 'weight',label: 'PESO MAXIMO lb',align: 'center',field: row => row.maxWeigth,format: val => `${val}`,sortable: true},
-  { name: 'units', align: 'center', label: 'UNIDADES POR CAJA', field: 'unitsPerBox',align: 'center', sortable: true },
+  { name: 'index', label: '#', field: 'index' },
+  { name: 'name', label: 'NOMBRE EMPAQUE', field: 'name', align: 'center' },
+  { name: 'weight', label: 'PESO MAXIMO lb', align: 'center', field: row => row.maxWeigth, format: val => `${val}`, sortable: true },
+  { name: 'units', align: 'center', label: 'UNIDADES POR CAJA', field: 'unitsPerBox', align: 'center', sortable: true },
+  { name: 'options', align: 'center', label: 'OPCIONES', align: 'center', sortable: true },
 ])
 
-let rows = ref([])
-rows.forEach((row, index) => {
+let rows = ref([
+  {name:"kadnska", maxWeight: 3,  unitsPerBox: 4}
+])
+rows.value.forEach((row, index) => {
   row.index = index
 })
 
-const postTypePackaing = async ()=>{
+const postTypePackaing = async () => {
   try {
-    const packaing = await axios.post(`http://localhost:3500/tipoEmpaque`,{
+    const packaing = await axios.post(`http://localhost:3500/tipoEmpaque`, {
       name: name.value,
       maxWeigth: maxWeight.value,
       unitsPerBox: units.value
@@ -87,17 +106,29 @@ const postTypePackaing = async ()=>{
     console.log(error);
   }
 }
-const getTypePackaing = async ()=>{
+const getTypePackaing = async () => {
   try {
     const packa = await axios.get(`http://localhost:3500/tipoEmpaque`)
     console.log(packa);
-    rows.value=packa.data
+    rows.value = packa.data
   } catch (error) {
     console.log(error);
   }
 }
 
-onMounted(()=>{
+
+// const putState = async () => {
+//   try {
+//     const packa = await axios.get(`http://localhost:3500/tipoEmpaque/${state}`,{
+//     }),
+//     console.log(packa);
+//     rows.value = packa.data
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+onMounted(() => {
   getTypePackaing()
 })
 
--- src/components/Transformation/HomeTransformation.vue
+++ src/components/Transformation/HomeTransformation.vue
@@ -9,41 +9,42 @@
         </div>
         <hr class="bg-green-10 q-mb-xl" style="width: 70%; height: 2px" />
 
-        <div class="row" style="margin-top: 240px">
+        <div class="row" style="margin-top: 200px">
             <div class="col-2"></div>
             <div class="col-xl-2 col-sm-2" >
+                <router-link to="/procesoDiario" style="text-decoration: none; color: black">
                 <q-card class="my-card" style="border-style: solid; border-color: green; border-radius: 10px">
                     <q-card-section class="text-center">
                         <span class="material-symbols-outlined" style="font-size: 60px;">
                             engineering
                         </span> </q-card-section>
                     <q-separator class="bg-green-10" style="height: 2px" />
-                    <router-link to="/procesoDiario" style="text-decoration: none; color: black">
                         <q-card-section>
                             <div class="text-center q-mt-md q-my-md">PROCESO DIARIO</div>
                         </q-card-section>
-                    </router-link>
-                </q-card>
+                    </q-card>
+                </router-link>
             </div>
 
             <div class="col-1"></div>
 
             <div class="col-xl-2 col-sm-2">
+                <router-link to="/materiaTransformada" style="text-decoration: none; color: black">
                 <q-card class="my-card" style="border-style: solid; border-color: green; border-radius: 10px">
                     <q-card-section class="text-center">
                         <span class="material-symbols-outlined" style="font-size: 60px;">
                             psychiatry
                         </span> </q-card-section>
                     <q-separator class="bg-green-10" style="height: 2px" />
-                    <router-link to="/materiaTransformada" style="text-decoration: none; color: black">
                         <q-card-section >
                             <div class="text-center q-mt-md">MATERIA PRIMA TRANSFORMADA</div>
                         </q-card-section>
-                    </router-link>
-                </q-card>
+                    </q-card>
+                </router-link>
             </div>
             <div class="col-1"></div>
             <div class="col-xl-2 col-sm-2">
+                <router-link to="/empacado" style="text-decoration: none; color: black">
                 <q-card class="my-card" style="border-style: solid; border-color: green; border-radius: 10px">
                     <q-card-section class="text-center">
                         <span class="material-symbols-outlined" style="font-size: 60px;">
@@ -51,12 +52,11 @@
                         </span>
                     </q-card-section>
                     <q-separator class="bg-green-10" style="height: 2px" />
-                    <router-link to="/empacado" style="text-decoration: none; color: black">
                         <q-card-section>
                             <div class="text-center q-mt-md q-my-md">EMPACADOS</div>
                         </q-card-section>
-                    </router-link>
-                </q-card>
+                    </q-card>
+                </router-link>
             </div>
             <div class="col-2"></div>
         </div>

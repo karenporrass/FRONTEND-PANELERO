98e89f07ac831214da70ead7caf4861d750d9459
Dani094 <murillofelipe094@gmail.com>
2023-05-23 11:29:39 -0500

    arreglo peticiones

--- src/store/Login/login.js
+++ src/store/Login/login.js
@@ -1,47 +1,45 @@
 import { defineStore } from 'pinia'
 import {ref} from "vue"
-import axios from "axios"
+import {requestAxios} from "../../Global/axios.js"
 
 export const LoginStore = defineStore('counter', () => {
-    const Login = ref("")
-    
-    async function listLogin() {
-      try {
-        return await axios.get("https://project-panelero.onrender.com/Login")
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    async function newLogin( user, password) {
-        try {
-            return await axios.post(`https://project-panelero.onrender.com/Login`,{
-              user: user,
-              password: password
-            })
-          } catch (error) {
-            console.log(error);
-          }
-      }
-
-      async function putLogin( id, user, password) {
-        try {
-            return await axios.post(`https://project-panelero.onrender.com/Login/${id}`,{
-                user: user,
-                password: password
-            })
-          } catch (error) {
-            console.log(error);
-          }
-      }
-
-    async function active(id, estado){
-      try {
-        return await axios.put(`https://project-panelero.onrender.com/Login/state/${id}`, {state:estado}) //asi es como se pasa por el body el state es como se llama en el backend y estado es el nombre de mi variable que le puse en la funcion
-      } catch (error) {
-        console.log(error);
-        return error
-      }
-    }
+  const login = ref("")
   
-    return { listLogin, Login, active, newLogin, putLogin }
-  })
+  async function listLogin() {
+    try {
+      return await requestAxios.get("/login")
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async function newLogin(name, attentionLine) {
+      try {
+          return await requestAxios.post(`/login`,{
+              name: name,
+              attentionLine: attentionLine
+          })
+        } catch (error) {
+          console.log(error);
+        }
+    }
+    async function putLogin(id ) {
+      try {
+          return await requestAxios.post(`/login/update/${id}`,{
+              id: id
+          })
+        } catch (error) {
+          console.log(error);
+        }
+    }
+
+  async function active(id, estado){
+    try {
+      return await requestAxios.put(`/login/update/state/${id}`, {state:estado}) //asi es como se pasa por el body el state es como se llama en el backend y estado es el nombre de mi variable que le puse en la funcion
+    } catch (error) {
+      console.log(error);
+      return error
+    }
+  }
+
+  return { listLogin, newLogin, putLogin, active, login }
+})

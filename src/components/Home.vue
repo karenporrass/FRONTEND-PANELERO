2d848bb6b165f234230563563b4b69da35b6226c
Yunquil <117084576+SandyGarciaR@users.noreply.github.com>
2023-06-23 12:08:07 -0500

    add kimages

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,25 +2,25 @@
     <div>
       <div class="row justify-center" style="margin-top: 40px;">
         <div class="col-10 text-center text-weight-bold text-h5">
-          <div>Imagenes corporativas</div>          
           <div>BIENVENIDOS</div>
+          <div>Imagenes corporativas</div>          
 
         </div>
       </div>
         <div class="row justify-center q-my-xl">
             <div class="col-10"  style="padding: 0px; ">
                 <div style="padding: 0px; ">
-    <q-carousel
+    <q-carousel 
       animated
       v-model="slide"
       arrows
       navigation
       infinite
+      style="height: 500px;"
     >
-      <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg" />
-      <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg" />
-      <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg" />
-      <q-carousel-slide :name="4" img-src="https://cdn.quasar.dev/img/quasar.jpg" />
+      <q-carousel-slide :name="1" img-src="/images/panela1.jpg" />
+      <q-carousel-slide :name="2" img-src="/images/panela2.jpg" />
+      <q-carousel-slide :name="3" img-src="/images/panela43.jpg" />
     </q-carousel>
   </div>
             </div>
